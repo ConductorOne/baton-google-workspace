c7ca18011322ffe7be889a2dd57082b99e0ffa42
John Allers <john.allers@conductorone.com>
2025-09-26 15:03:11 -0400

    fix lint issue

--- pkg/connector/group.go
+++ pkg/connector/group.go
@@ -69,13 +69,15 @@ func (o *groupResourceType) List(ctx context.Context, resourceId *v2.ResourceId,
 	if err != nil {
 		return nil, "", nil, fmt.Errorf("google-workspace: cannot get groups: %w", err)
 	}
+
+	serverResponse := groups.ServerResponse
 	if len(groups.Groups) == 0 {
-		l.Warn("no groups found", zap.Int("status", groups.ServerResponse.HTTPStatusCode),
-			zap.Any("header", groups.ServerResponse.Header), zap.String("req", bag.PageToken()))
+		l.Warn("no groups found", zap.Int("status", serverResponse.HTTPStatusCode),
+			zap.Any("header", serverResponse.Header), zap.String("req", bag.PageToken()))
 	} else {
 		l.Debug("groups found", zap.Int("count", len(groups.Groups)),
-			zap.Int("status", groups.ServerResponse.HTTPStatusCode),
-			zap.Any("header", groups.ServerResponse.Header), zap.String("req", bag.PageToken()))
+			zap.Int("status", serverResponse.HTTPStatusCode),
+			zap.Any("header", serverResponse.Header), zap.String("req", bag.PageToken()))
 	}
 
 	rv := make([]*v2.Resource, 0, len(groups.Groups))
