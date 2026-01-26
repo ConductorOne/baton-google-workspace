f743dec706b33190ff54bc02a4d6b692a4ce91af
agustin-conductor <agustin.sosa@conductorone.com>
2026-01-26 13:21:38 -0300

    update baton sdk to v0.7.1 (#68)
    * update baton sdk to v0.7.1
    
    * increase sleep time after delete for ci test

--- vendor/github.com/conductorone/baton-sdk/pkg/sync/syncer.go
+++ vendor/github.com/conductorone/baton-sdk/pkg/sync/syncer.go
@@ -12,6 +12,8 @@ import (
 	"slices"
 	"strconv"
 	"strings"
+	native_sync "sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/Masterminds/semver/v3"
@@ -253,6 +255,8 @@ type syncer struct {
 	injectSyncIDAnnotation              bool
 	setSessionStore                     sessions.SetSessionStore
 	syncResourceTypes                   []string
+	previousSyncMu                      native_sync.Mutex
+	previousSyncIDPtr                   atomic.Pointer[string]
 }
 
 const minCheckpointInterval = 10 * time.Second
@@ -276,6 +280,33 @@ func (s *syncer) Checkpoint(ctx context.Context, force bool) error {
 	}
 
 	return nil
+}
+
+func (s *syncer) getPreviousFullSyncID(ctx context.Context) (string, error) {
+	if ptr := s.previousSyncIDPtr.Load(); ptr != nil {
+		return *ptr, nil
+	}
+
+	s.previousSyncMu.Lock()
+	defer s.previousSyncMu.Unlock()
+
+	if ptr := s.previousSyncIDPtr.Load(); ptr != nil {
+		return *ptr, nil
+	}
+
+	psf, ok := s.store.(latestSyncFetcher)
+	if !ok {
+		empty := ""
+		s.previousSyncIDPtr.Store(&empty)
+		return "", nil
+	}
+
+	previousSyncID, err := psf.LatestFinishedSync(ctx, connectorstore.SyncTypeFull)
+	if err == nil {
+		s.previousSyncIDPtr.Store(&previousSyncID)
+	}
+
+	return previousSyncID, err
 }
 
 func (s *syncer) handleInitialActionForStep(ctx context.Context, a Action) {
@@ -876,6 +907,12 @@ func validateSyncResourceTypesFilter(resourceTypesFilter []string, validResource
 	return nil
 }
 
+func (s *syncer) hasChildResources(resource *v2.Resource) bool {
+	annos := annotations.Annotations(resource.GetAnnotations())
+
+	return annos.Contains((*v2.ChildResourceType)(nil))
+}
+
 // getSubResources fetches the sub resource types from a resources' annotations.
 func (s *syncer) getSubResources(ctx context.Context, parent *v2.Resource) error {
 	ctx, span := tracer.Start(ctx, "syncer.getSubResources")
@@ -1097,21 +1134,38 @@ func (s *syncer) syncResources(ctx context.Context) error {
 
 	bulkPutResoruces := []*v2.Resource{}
 	for _, r := range resp.GetList() {
+		validatedResource := false
+
 		// Check if we've already synced this resource, skip it if we have
 		_, err = s.store.GetResource(ctx, reader_v2.ResourcesReaderServiceGetResourceRequest_builder{
 			ResourceId: v2.ResourceId_builder{ResourceType: r.GetId().GetResourceType(), Resource: r.GetId().GetResource()}.Build(),
 		}.Build())
 		if err == nil {
-			continue
+			err = s.validateResourceTraits(ctx, r)
+			if err != nil {
+				return err
+			}
+			validatedResource = true
+
+			// We must *ALSO* check if we have any child resources.
+			if !s.hasChildResources(r) {
+				// Since we only have the resource type IDs of child resources,
+				// we can't tell if we already have synced those child resources.
+				// Those children may also have their own child resources,
+				// so we are conservative here and just re-sync this resource.
+				continue
+			}
 		}
 
-		if !errors.Is(err, sql.ErrNoRows) {
+		if err != nil && !errors.Is(err, sql.ErrNoRows) {
 			return err
 		}
 
-		err = s.validateResourceTraits(ctx, r)
-		if err != nil {
-			return err
+		if !validatedResource {
+			err = s.validateResourceTraits(ctx, r)
+			if err != nil {
+				return err
+			}
 		}
 
 		// Set the resource creation source
@@ -1893,14 +1947,9 @@ func (s *syncer) fetchResourceForPreviousSync(ctx context.Context, resourceID *v
 
 	l := ctxzap.Extract(ctx)
 
-	var previousSyncID string
-	var err error
-
-	if psf, ok := s.store.(latestSyncFetcher); ok {
-		previousSyncID, err = psf.LatestFinishedSync(ctx, connectorstore.SyncTypeFull)
-		if err != nil {
-			return "", nil, err
-		}
+	previousSyncID, err := s.getPreviousFullSyncID(ctx)
+	if err != nil {
+		return "", nil, err
 	}
 
 	if previousSyncID == "" {
@@ -1965,6 +2014,7 @@ func (s *syncer) fetchEtaggedGrantsForResource(
 	var ret []*v2.Grant
 
 	// No previous etag, so an etag match is not possible
+	// TODO(kans): do the request again to get the grants, but this time don't use the etag match!
 	if prevEtag == nil {
 		return nil, false, errors.New("connector returned an etag match but there is no previous sync generation to use")
 	}
